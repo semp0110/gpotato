3a598dbe9dd3bbf8c1d2c21d85443705cacd9c6e
박희랑 [hirang] <hirang@nexon.co.kr>
2017-08-11 19:09:37 +0900

    gpotato init

---
+++ gpotato_db/src/main/java/com/gpotato/db/GpotatoDbApplication.java
@@ -0,0 +1,12 @@
+package com.gpotato.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpotatoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpotatoDbApplication.class, args);
+	}
+}
